d9a5a51664c2e049a5a497816df5d13901d58c6b
Bartolomeo77 <maker.tk7@gmail.com>
2023-06-28 07:14:47 -0700

    Descripción de los cambios realizados

--- mcfly/ViewController.swift
+++ mcfly/ViewController.swift
@@ -8,9 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet var segmentlogeo: UISegmentedControl!
+    @IBOutlet var sglogear: UIView!
+    @IBOutlet var sgregister: UIView!
+    
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            self.view.bringSubviewToFront(sglogear)
+        case 1:
+            self.view.bringSubviewToFront(sgregister)
+        default:
+            break
+        }
+        
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.view.bringSubviewToFront(sglogear)
         // Do any additional setup after loading the view.
     }
 
